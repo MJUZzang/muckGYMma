eb9ac01e2825bb017bf46e4876b355d926280c11
devholic22 <hyunjoon.tech@gmail.com>
2024-03-12 16:27:52 +0900

    initial commit be

---
+++ projects/paygo/src/main/java/mju/paygo/PaygoApplication.java
@@ -0,0 +1,13 @@
+package mju.paygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaygoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaygoApplication.class, args);
+	}
+
+}
---
+++ projects/paygo/src/test/java/mju/paygo/PaygoApplicationTests.java
@@ -0,0 +1,13 @@
+package mju.paygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaygoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
