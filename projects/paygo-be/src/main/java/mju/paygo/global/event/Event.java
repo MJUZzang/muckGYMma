ca639cc8108b9a857363744d5d691381ba05ae00
devholic22 <hyunjoon.tech@gmail.com>
2024-03-14 22:06:13 +0900

    chore: global 설정 진행
    - 이벤트 설정 (EventsConfiguration)
    - Cors 설정 (CorsCustomFilter)
    - Jasypt 설정 (JasyptConfig)
    - WebMvc 설정 (WebMvcConfig)
    - 이벤트 설정 (Event, Events)
    - global 예외 설정 (GlobalExceptionHandler)

---
+++ projects/paygo-be/src/main/java/mju/paygo/global/event/Event.java
@@ -0,0 +1,13 @@
+package mju.paygo.global.event;
+
+import lombok.Getter;
+
+@Getter
+public abstract class Event {
+
+    private final Long timestamp;
+
+    protected Event() {
+        this.timestamp = System.currentTimeMillis();
+    }
+}
