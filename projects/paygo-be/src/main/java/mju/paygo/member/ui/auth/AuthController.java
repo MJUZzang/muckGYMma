d94533b7ebfe3aa32c82375516881243c5a838a0
devholic22 <hyunjoon.tech@gmail.com>
2024-03-14 22:27:00 +0900

    feat: 회원 컨트롤러 구현
    - 로그인 기능을 외부에서 처리할 컨트롤러 설정
    - 컨트롤러 전 필요한 필터와 인터셉터 구현

---
+++ projects/paygo-be/src/main/java/mju/paygo/member/ui/auth/AuthController.java
@@ -0,0 +1,48 @@
+package mju.paygo.member.ui.auth;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import mju.paygo.member.application.auth.AuthService;
+import mju.paygo.member.application.auth.dto.LoginRequest;
+import mju.paygo.member.infrastructure.auth.dto.OAuthProviderRequest;
+import mju.paygo.member.ui.auth.dto.TokenResponse;
+import mju.paygo.member.ui.auth.support.auth.OAuthAuthority;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Value("${jwt.expiration-period}")
+    private String expiration;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody @Valid final LoginRequest request,
+                                               @OAuthAuthority final OAuthProviderRequest provider,
+                                               final HttpServletResponse response) {
+        String token = authService.login(request, provider);
+        response.addCookie(generateCookieWithToken(token));
+
+        return ResponseEntity.ok(new TokenResponse(token));
+    }
+
+    private Cookie generateCookieWithToken(final String token) {
+        Cookie cookie = new Cookie("token", token);
+        cookie.setPath("/");
+        cookie.setSecure(true);
+        cookie.setMaxAge(Integer.parseInt(expiration));
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+}
