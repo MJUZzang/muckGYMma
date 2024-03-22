f171595820658baa74f887d56abfe10938540477
Jehee Cheon <jeheecheon@gmail.com>
2024-03-22 10:35:30 +0900

    chore: darkmode 코드 분리

---
+++ projects/fe/src/app/_components/DarkMode.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+const DarkMode = () => {
+    useEffect(() => {
+        const darkMode =
+            localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+        if (darkMode) {
+            document.documentElement.classList.add("dark");
+            localStorage.theme = "dark";
+        } else {
+            localStorage.theme = "light";
+            document.documentElement.classList.remove("dark");
+        }
+    }, []);
+    return null;
+};
+
+export default DarkMode;
--- projects/fe/src/app/layout.tsx
+++ projects/fe/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/_styles/globals.css";
 import NavBar from "@/app/_components/NavBar";
+import DarkMode from "@/app/_components/DarkMode";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
             <body className={`flex flex-col h-screen ${inter.className}`}>
                 <div className="h-full">{children}</div>
                 <NavBar />
+                <DarkMode />
             </body>
         </html>
     );
