94f42b8aab1889899a8dbadb7d1fb630bfe70d65
Jehee Cheon <jeheecheon@gmail.com>
2024-03-23 04:01:47 +0900

    feat: post 컴포넌트 레이아웃 완성

--- projects/fe/src/app/community/following/_components/Post.tsx
+++ projects/fe/src/app/community/following/_components/Post.tsx
@@ -1,6 +1,9 @@
 import PostInfo from "@/app/_types/PostInfo";
 import Image from "next/image";
 import React from "react";
+import Like from "./_images/Like";
+import Comment from "./_images/Comment";
+import SpoonKnife from "./_images/SpoonKnife";
 
 interface PostProps {
     postInfo: PostInfo;
@@ -8,7 +11,7 @@ interface PostProps {
 
 const Post: React.FC<PostProps> = ({ postInfo }) => {
     return (
-        <>
+        <div className="bg-[#f1f1f1]">
             <div className="mx-3 flex flex-col py-3">
                 {/* 유저 정보 */}
                 <div className="flex">
@@ -30,16 +33,24 @@ const Post: React.FC<PostProps> = ({ postInfo }) => {
             </div>
 
             {/* 포스트 이미지 */}
-            <Image src={postInfo.image} alt="Post image" className="w-full rounded-t-lg" />
+            <Image
+                src={postInfo.image}
+                alt="Post image"
+                className="w-full rounded-t-lg"
+            />
 
             {/* 포스트 정보 */}
-            <div className="flex justify-between text-sm text-gray-500 mx-3">
+            <div className="flex justify-between text-sm text-gray-500 mx-3 py-2">
                 <p>{postInfo.likes} liked</p>
                 <p>{postInfo.comments} comments</p>
             </div>
 
-            
-        </>
+            <div className="flex justify-between border-t-2 mx-2 py-2">
+                <Like className="basis-1/3" />
+                <Comment className="basis-1/3" />
+                <SpoonKnife className="basis-1/3" />
+            </div>
+        </div>
     );
 };
 
--- projects/fe/src/app/community/following/page.tsx
+++ projects/fe/src/app/community/following/page.tsx
@@ -3,27 +3,41 @@ import Post from "@/app/community/following/_components/Post";
 import exampleImage from "@/app/_images/pooh.jpg";
 
 const Follwing = () => {
+    const dummyPost = {
+        comments: 0,
+        content: "Hello World",
+        hasLiked: false,
+        id: 1,
+        image: exampleImage,
+        likes: 0,
+        postedAt: new Date(),
+        user: {
+            name: "John Doe",
+            avatar: exampleImage,
+        },
+    };
+
     return (
         <>
             <div className="border-b-2">
                 <p className="mx-3">Suggested Posts</p>
             </div>
 
-            <Post
-                postInfo={{
-                    comments: 0,
-                    content: "Hello World",
-                    hasLiked: false,
-                    id: 1,
-                    image: exampleImage,
-                    likes: 0,
-                    postedAt: new Date(),
-                    user: {
-                        name: "John Doe",
-                        avatar: exampleImage,
-                    },
-                }}
-            />
+            <div className="flex flex-col gap-5">
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+            <Post postInfo={dummyPost} />
+
+            </div>
         </>
     );
 };
