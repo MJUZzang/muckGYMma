3e0a2e7b3c3785e9b8f5802800e308e0c504a7f7
Jehee Cheon <jeheecheon@gmail.com>
2024-03-23 02:57:20 +0900

    refactor: HeaderBar 불투명한 것 수정

--- projects/fe/src/app/_components/HeaderBar.tsx
+++ projects/fe/src/app/_components/HeaderBar.tsx
@@ -7,7 +7,8 @@ interface HeaderBarProps {
 
 const HeaderBar: React.FC<HeaderBarProps> = (props) => {
     return (
-        <div className={`fixed px-2 py-1 h-[40px] w-full flex flex-row items-center border-b-2 text-2xl
+        <div className={`fixed px-3 py-1 h-[40px] w-full flex flex-row items-center border-b-2 text-2xl
+        bg-white dark:bg-black z-10
             ${props.className}`}>
             {props.children}
         </div>
