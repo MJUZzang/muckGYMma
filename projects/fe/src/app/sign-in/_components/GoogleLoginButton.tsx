44e9426199bf334c913aca41a1f56aa5abbdbde4
Jehee Cheon <jeheecheon@gmail.com>
2024-03-23 19:47:41 +0900

    feat: 로그인 버튼 동작하도록 수정

--- projects/fe/src/app/sign-in/_components/GoogleLoginButton.tsx
+++ projects/fe/src/app/sign-in/_components/GoogleLoginButton.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import React from "react";
+import { useRouter } from "next/navigation";
+
 import LoginButton from "@/app/sign-in/_components/LoginButton";
 import GoogleSymbol from "@/app/sign-in/_images/GoogleSymbol";
+import { googleLoginUrl } from "@/app/sign-in/_utils/login";
 
-const GoogleLoginButton = () => {
+interface GoogleLoginButtonProps {
+    onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = (props) => {
+    const router = useRouter();
+
     return (
         <LoginButton
+            onClick={() => router.push(googleLoginUrl)}
             className="bg-[#FEFEFE]"
             title="Continue with Google"
             Symbol={GoogleSymbol}
--- projects/fe/src/app/sign-in/page.tsx
+++ projects/fe/src/app/sign-in/page.tsx
@@ -1,37 +1,35 @@
 import React from "react";
 
-import KakaoSymbol from "@/app/sign-in/_images/KakaoSymbol";
-import GoogleSymbol from "@/app/sign-in/_images/GoogleSymbol";
 import GoogleLoginButton from "./_components/GoogleLoginButton";
 import KakaoLoginButton from "./_components/KakaoLoginButton";
 
 const page = () => {
     return (
-        <main
-            className="absolute w-screen h-screen overflow-y-auto bg-[#212121] dark:bg-[#050505]
-              px-4"
-        >
-            <div className="h-1/2 flex flex-col justify-end">
-                <div className="italic font-extrabold text-2xl text-white">
-                    <p>AI - Powered</p>
-                    <p>Workout Coaching</p>
-                    <p>Tailored To You</p>
+        <main className="absolute w-screen h-screen overflow-y-auto bg-[#212121] dark:bg-[#050505]">
+            <div className="px-4 max-w-[500px] w-full h-full mx-auto">
+                <div className="h-1/2 flex flex-col justify-end">
+                    <div className="italic font-extrabold text-2xl text-white">
+                        <p>AI - Powered</p>
+                        <p>Workout Coaching</p>
+                        <p>Tailored To You</p>
+                    </div>
+                    <p className="text-gray-300 text-sm py-5">
+                        Get personalized plan in 1 min.
+                    </p>
                 </div>
-                <p className="text-gray-300 text-sm py-5">
-                    Get personalized plan in 1 min.
-                </p>
-            </div>
-            <div className="h-1/2 flex flex-col justify-between">
-                <div className="space-y-4">
-                    <KakaoLoginButton />
-                    <GoogleLoginButton />
-                </div>
+                
+                <div className="h-1/2 flex flex-col justify-between">
+                    <div className="space-y-4">
+                        <KakaoLoginButton />
+                        <GoogleLoginButton />
+                    </div>
 
-                <div className="text-gray-400 flex flex-col items-center gap-2 text-xs mb-6">
-                    <p>By logging in, you agree to the terms below.</p>
-                    <div className="flex justify-center gap-10 underline underline-offset-2">
-                        <p>Privacy Policy</p>
-                        <p>Terms of Use</p>
+                    <div className="text-gray-400 flex flex-col items-center gap-2 text-xs mb-6">
+                        <p>By logging in, you agree to the terms below.</p>
+                        <div className="flex justify-center gap-10 underline underline-offset-2">
+                            <p>Privacy Policy</p>
+                            <p>Terms of Use</p>
+                        </div>
                     </div>
                 </div>
             </div>
