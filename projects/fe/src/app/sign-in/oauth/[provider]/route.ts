46e8a56eb557382a1cbf6eeb4f71aafc74ad217f
Jehee Cheon <jeheecheon@gmail.com>
2024-04-05 02:21:13 +0900

    feat: 'sign-in/oauth/[provider]' endpoint 생성

---
+++ projects/fe/src/app/sign-in/oauth/[provider]/route.ts
@@ -0,0 +1,36 @@
+import { redirect } from "next/navigation";
+import { type NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic"; // defaults to auto
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { provider: string } }
+) {
+    const searchParams = request.nextUrl.searchParams;
+    const code = searchParams.get("code");
+    const provider = params.provider;
+
+    console.log("code", code);
+    console.log('provider', provider);
+
+    fetch('http://localhost:8080/api/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code, provider }),
+    })
+        .then((res) => {
+            if (res.ok) {
+                console.log("Logged in successfully");
+            } else {
+                throw new Error("Failed to login");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    redirect("/");
+}
