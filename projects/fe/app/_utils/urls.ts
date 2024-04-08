7ae9566c45da04b2bf6360017575e7bf9398a952
Jehee Cheon <jeheecheon@gmail.com>
2024-04-08 18:48:31 +0900

    refactor: 로그인 엔드포인트 제거 후 페이지로 변경

---
+++ projects/fe/app/_utils/urls.ts
@@ -0,0 +1,9 @@
+export const backendUrlDev = "http://localhost:8080";
+export const backendUrlProd = "https://muckgymma.p-e.kr";
+export const backendUrl =
+    process.env.NODE_ENV === "development" ? backendUrlDev : backendUrlProd;
+
+export const frontUrlDev = "https://localhost:3000";
+export const frontUrlProd = "https://muckgymma.kro.kr";
+export const frontUrl =
+    process.env.NODE_ENV === "development" ? frontUrlDev : frontUrlProd;
---
+++ projects/fe/app/sign-in/oauth/[provider]/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { backendUrl } from "@/_utils/urls";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const Page = () => {
+    const params = useParams();
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    useEffect(() => {
+        const code = searchParams.get("code");
+        const provider = params.provider;
+
+        fetch(`${backendUrl}/api/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ code, provider }),
+        })
+            .then((res) => {
+                if (res.ok) {
+                    console.log("Logged in successfully");
+                    router.push("/");
+                } else {
+                    throw new Error("Failed to login");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+    return null;
+};
+
+export default Page;
