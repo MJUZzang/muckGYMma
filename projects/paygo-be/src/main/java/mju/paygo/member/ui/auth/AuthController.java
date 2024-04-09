2416c92291b7da7c572d835c37c832e5c6bfc45e
devholic22 <hyunjoon.tech@gmail.com>
2024-04-09 16:25:12 +0900

    fix: CORS 교정

--- projects/paygo-be/src/main/java/mju/paygo/member/ui/auth/AuthController.java
+++ projects/paygo-be/src/main/java/mju/paygo/member/ui/auth/AuthController.java
@@ -11,11 +11,13 @@ import mju.paygo.member.ui.auth.dto.TokenResponse;
 import mju.paygo.member.ui.auth.support.auth.OAuthAuthority;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://muckgymma.kro.kr", allowedHeaders = "GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH")
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
