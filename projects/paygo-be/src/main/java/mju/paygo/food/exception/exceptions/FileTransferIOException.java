2df2294ee4d0b85969163d4307c0df372559b40e
devholic22 <hyunjoon.tech@gmail.com>
2024-04-12 16:47:35 +0900

    feat: S3 연동 구현
    - 음식 사진 업로드 시 바로 S3으로 연동되도록 설정
    - 회원 id는 오류가 발생하기 때문에 임시 데이터 작성

---
+++ projects/paygo-be/src/main/java/mju/paygo/food/exception/exceptions/FileTransferIOException.java
@@ -0,0 +1,8 @@
+package mju.paygo.food.exception.exceptions;
+
+public class FileTransferIOException extends RuntimeException {
+
+    public FileTransferIOException() {
+        super("파일 전송 과정에서 입출력 예외가 발생하였습니다.");
+    }
+}
