bda0d7629eb8112536c5934a5a30fe88b5455928
Jehee Cheon <jeheecheon@gmail.com>
2024-04-14 00:56:36 +0900

    feat: 사진 안의 음식 중 섭취한 것만 선택하여 분석되도록 수정

--- projects/fe/app/prediction/pick-one/_components/ConfirmModal.tsx
+++ projects/fe/app/prediction/pick-one/_components/ConfirmModal.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
+import { PredictState } from "@/../lib/slices/predictSlice";
+import { backendUrl } from "@/_utils/urls";
 
 interface Props {
     title: string;
     onOkClicked: () => void;
     onCancelClicked: () => void;
     isModalOpen: boolean;
+    predict: PredictState;
+    selectedPredict: number;
 }
 
 function ConfirmModal({
@@ -12,26 +16,98 @@ function ConfirmModal({
     onOkClicked,
     onCancelClicked,
     isModalOpen,
+    predict,
+    selectedPredict,
 }: Props) {
+    const [foodsSelected, setFoodsSelected] = useState<number[]>([]);
+
+    const handleOkClicked: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.stopPropagation();
+        onOkClicked();
+
+        if (!predict.predictlist) {
+            console.error("predictlist is not exist");
+            return;
+        }
+
+        fetch(`${backendUrl}/api/food/pick`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                id: foodsSelected,
+                imageUrl: predict.fileUrl,
+            }),
+        })
+            .then((res) => {
+                console.log(res.status);
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    console.error("fail");
+                }
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => console.error(err));
+    };
+
     return (
         <div
             className={`absolute w-full px-[8vw] top-[40vh] transition-all duration-500
-        ${isModalOpen ? "opacity-100 z-[50]" : "opacity-0"}`}
+            ${isModalOpen ? "opacity-100 z-[50]" : "opacity-0"}`}
+            onClick={(e) => e.stopPropagation()}
         >
-            <div className="bg-black/90 rounded-xl w-full h-[20vh] py-6 flex flex-col">
+            <div className="bg-black/90 rounded-xl w-full py-6 flex flex-col">
                 <p className="block text-center text-xl text-fluorescent">
                     {title}
                 </p>
+
+                <div className="text-white my-5 flex flex-col gap-3">
+                    {predict.predictlist &&
+                        predict.predictlist[selectedPredict].foodlist.map(
+                            (food, idx) => (
+                                <div
+                                    key={food.id}
+                                    className="flex justify-between px-5"
+                                >
+                                    <p>{food.foodname}</p>
+                                    <input
+                                        type="checkbox"
+                                        checked={foodsSelected.includes(
+                                            food.id
+                                        )}
+                                        onChange={() => {
+                                            if (
+                                                foodsSelected.includes(food.id)
+                                            ) {
+                                                setFoodsSelected(
+                                                    foodsSelected.filter(
+                                                        (id) => id !== food.id
+                                                    )
+                                                );
+                                            } else {
+                                                setFoodsSelected([
+                                                    ...foodsSelected,
+                                                    food.id,
+                                                ]);
+                                            }
+                                        }}
+                                    />
+                                </div>
+                            )
+                        )}
+                </div>
+
                 <div className="mt-auto flex justify-center gap-5">
                     <button
                         className="bg-fluorescent px-5 py-2 rounded-lg font-bold"
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            e.stopPropagation();
-                            onOkClicked();
-                        }}
+                        onClick={handleOkClicked}
                     >
-                        선택하기
+                        분석하기
                     </button>
                     <button
                         className="bg-white text-black px-5 py-2 rounded-lg font-extrabold"
@@ -45,4 +121,4 @@ function ConfirmModal({
     );
 }
 
-export default ConfirmModal;
+export default React.memo(ConfirmModal);
--- projects/fe/app/prediction/pick-one/page.tsx
+++ projects/fe/app/prediction/pick-one/page.tsx
@@ -2,90 +2,89 @@
 
 import React, { useState } from "react";
 import { useAppSelector } from "@/../lib/hooks";
-import { PredictState, selectPredict } from "@/../lib/slices/predictSlice";
+import { selectPredict } from "@/../lib/slices/predictSlice";
 import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
 import ConfirmModal from "./_components/ConfirmModal";
-import { backendUrl } from "@/_utils/urls";
 
-const dummyData: PredictState = {
-    predictlist: [
-        {
-            keyname: "roasted_pork_belly",
-            foodlist: [
-                {
-                    id: 690,
-                    foodname: "돼지삼겹살(구운것)",
-                    manufacturer: "",
-                    predict_key: "roasted_pork_belly",
-                },
-            ],
-            possibility: 99.98961687088013,
-        },
-        {
-            keyname: "smoked_pork_belly",
-            foodlist: [
-                {
-                    id: 11116,
-                    foodname: "훈제삼겹살",
-                    manufacturer: "",
-                    predict_key: "smoked_pork_belly",
-                },
-            ],
-            possibility: 0.005348741251509637,
-        },
-        {
-            keyname: "bossam",
-            foodlist: [
-                {
-                    id: 820,
-                    foodname: "돼지고기보쌈(사태)",
-                    manufacturer: "",
-                    predict_key: "bossam",
-                },
-                {
-                    id: 821,
-                    foodname: "돼지고기보쌈(삼겹살)",
-                    manufacturer: "",
-                    predict_key: "bossam",
-                },
-            ],
-            possibility: 0.0026325657017878257,
-        },
-        {
-            keyname: "steak_rice",
-            foodlist: [
-                {
-                    id: 80,
-                    foodname: "스테이크덮밥",
-                    manufacturer: "",
-                    predict_key: "steak_rice",
-                },
-            ],
-            possibility: 0.0006327998562483117,
-        },
-        {
-            keyname: "five_spice_sliced_pork",
-            foodlist: [
-                {
-                    id: 894,
-                    foodname: "오향장육",
-                    manufacturer: "",
-                    predict_key: "five_spice_sliced_pork",
-                },
-            ],
-            possibility: 0.00041311200220661703,
-        },
-    ],
-    fileUrl:
-        "https://muckgymma.s3.ap-northeast-2.amazonaws.com/food/1_%25EC%2582%25BC%25EA%25B2%25B9%25EC%2582%25B4.jpg",
-};
+// const dummyData = {
+//     predictlist: [
+//         {
+//             keyname: "roasted_pork_belly",
+//             foodlist: [
+//                 {
+//                     id: 690,
+//                     foodname: "돼지삼겹살(구운것)",
+//                     manufacturer: "",
+//                     predict_key: "roasted_pork_belly",
+//                 },
+//             ],
+//             possibility: 99.98961687088013,
+//         },
+//         {
+//             keyname: "smoked_pork_belly",
+//             foodlist: [
+//                 {
+//                     id: 11116,
+//                     foodname: "훈제삼겹살",
+//                     manufacturer: "",
+//                     predict_key: "smoked_pork_belly",
+//                 },
+//             ],
+//             possibility: 0.005348741251509637,
+//         },
+//         {
+//             keyname: "bossam",
+//             foodlist: [
+//                 {
+//                     id: 820,
+//                     foodname: "돼지고기보쌈(사태)",
+//                     manufacturer: "",
+//                     predict_key: "bossam",
+//                 },
+//                 {
+//                     id: 821,
+//                     foodname: "돼지고기보쌈(삼겹살)",
+//                     manufacturer: "",
+//                     predict_key: "bossam",
+//                 },
+//             ],
+//             possibility: 0.0026325657017878257,
+//         },
+//         {
+//             keyname: "steak_rice",
+//             foodlist: [
+//                 {
+//                     id: 80,
+//                     foodname: "스테이크덮밥",
+//                     manufacturer: "",
+//                     predict_key: "steak_rice",
+//                 },
+//             ],
+//             possibility: 0.0006327998562483117,
+//         },
+//         {
+//             keyname: "five_spice_sliced_pork",
+//             foodlist: [
+//                 {
+//                     id: 894,
+//                     foodname: "오향장육",
+//                     manufacturer: "",
+//                     predict_key: "five_spice_sliced_pork",
+//                 },
+//             ],
+//             possibility: 0.00041311200220661703,
+//         },
+//     ],
+//     fileUrl:
+//         "https://muckgymma.s3.ap-northeast-2.amazonaws.com/food/1_%25EC%2582%25BC%25EA%25B2%25B9%25EC%2582%25B4.jpg",
+// };
 
-if (dummyData.predictlist) {
-    for (let predict of dummyData.predictlist) {
-        predict.possibility = parseFloat(predict.possibility.toFixed(5));
-    }
-}
+// if (dummyData.predictlist) {
+//     for (let predict of dummyData.predictlist) {
+//         predict.possibility = parseFloat(predict.possibility.toFixed(5));
+//     }
+// }
 
 export default function Page() {
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
@@ -95,50 +94,15 @@ export default function Page() {
     const predict = useAppSelector(selectPredict);
     // const predict = dummyData;
 
-    function handleOkClicked() {
-        setIsModalOpen(false);
-        if (!predict.predictlist) {
-            console.error("predictlist is not exist");
-            return;
-        }
-
-        const foodIdList: number[] = [];
-        predict.predictlist[selectedPredict].foodlist.forEach((food) => {
-            foodIdList.push(food.id);
-        });
-
-        fetch(`${backendUrl}/api/food/pick`, {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                id: foodIdList,
-                imageUrl: predict.fileUrl
-            }),
-        })
-            .then((res) => {
-                console.log(res.status);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    console.error("fail");
-                }
-            })
-            .then((data) => {
-                console.log(data);
-            })
-            .catch((err) => console.error(err));
-    }
-
     return (
         <div className="h-full" onClick={() => setIsModalOpen(false)}>
             <ConfirmModal
-                title={`${selectedPredict + 1}번을 선택하시겠습니까?`}
-                onOkClicked={handleOkClicked}
+                title="섭취한 음식을 선택해주세요"
+                onOkClicked={() => setIsModalOpen(false)}
                 onCancelClicked={() => setIsModalOpen(false)}
                 isModalOpen={isModalOpen}
+                predict={predict}
+                selectedPredict={selectedPredict}
             />
 
             <p className="block mb-2 text-center text-white/90 mx-auto text-2xl">
@@ -164,6 +128,7 @@ export default function Page() {
                             px-2 first:ml-[10vw] last:mr-[10vw] ml-[5vw] w-[80vw]"
                             onClick={(e) => {
                                 e.stopPropagation();
+                                setSelectedPredict(idx);
                                 setIsModalOpen(true);
                             }}
                         >
@@ -175,7 +140,6 @@ export default function Page() {
                                 {item.foodlist.map((food) => (
                                     <div key={food.id}>
                                         <p>{food.foodname}</p>
-                                        {/* <p>{food.manufacturer}</p> */}
                                     </div>
                                 ))}
                             </div>
