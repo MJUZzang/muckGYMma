816abc937d6c94aea5b6d4b8d85e9fbaa2bc4ca3
devholic22 <hyunjoon.tech@gmail.com>
2024-04-14 20:42:31 +0900

    refactor: 오류 메시지 반환 추가
    - ExceptionHandler 추가하여 오류 메시지 반환하도록 수정
    - 오류 메시지 응답 dto 추가
    - OAuthPlatformNotFoundException 오타 수정
    - 모든 예외 처리 적용

---
+++ projects/paygo-be/src/main/java/mju/paygo/member/exception/exceptions/MemberExceptionHandler.java
@@ -0,0 +1,52 @@
+package mju.paygo.member.exception.exceptions;
+
+import mju.paygo.global.exception.dto.ExceptionResponse;
+import mju.paygo.member.exception.exceptions.auth.ExpiredTokenException;
+import mju.paygo.member.exception.exceptions.auth.LoginInvalidException;
+import mju.paygo.member.exception.exceptions.auth.OAuthAccessException;
+import mju.paygo.member.exception.exceptions.auth.OAuthPlatformNotFoundException;
+import mju.paygo.member.exception.exceptions.auth.SignatureInvalidException;
+import mju.paygo.member.exception.exceptions.auth.TokenFormInvalidException;
+import mju.paygo.member.exception.exceptions.auth.TokenInvalidException;
+import mju.paygo.member.exception.exceptions.auth.UnsupportedTokenException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestControllerAdvice
+public class MemberExceptionHandler {
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpClientErrorException(final HttpClientErrorException e) {
+        return getBadRequest(new OAuthAccessException());
+    }
+
+    @ExceptionHandler(OAuthPlatformNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleOAuthPlatformNotFoundException(final OAuthPlatformNotFoundException e) {
+        return getNotFound(e);
+    }
+
+    @ExceptionHandler(value = {
+            ExpiredTokenException.class,
+            LoginInvalidException.class,
+            SignatureInvalidException.class,
+            TokenFormInvalidException.class,
+            TokenInvalidException.class,
+            UnsupportedTokenException.class
+    })
+    public ResponseEntity<ExceptionResponse> handleAuthException(final Exception e) {
+        return getBadRequest(e);
+    }
+
+    private ResponseEntity<ExceptionResponse> getBadRequest(final Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(e.getMessage()));
+    }
+
+    private ResponseEntity<ExceptionResponse> getNotFound(final Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(e.getMessage()));
+    }
+}
--- projects/paygo-be/src/main/java/mju/paygo/member/exception/exceptions/auth/OAuthPlatformNotFoundException.java
+++ projects/paygo-be/src/main/java/mju/paygo/member/exception/exceptions/auth/OAuthPlatformNotFoundException.java
@@ -1,8 +1,8 @@
 package mju.paygo.member.exception.exceptions.auth;
 
-public class OAuthPlatformNotFountException extends RuntimeException {
+public class OAuthPlatformNotFoundException extends RuntimeException {
 
-    public OAuthPlatformNotFountException() {
+    public OAuthPlatformNotFoundException() {
         super("소셜 로그인 기관을 찾을 수 없습니다.");
     }
 }
