8caac450aff696dfbc1716b98d16c8d2beb5efb2
devholic22 <hyunjoon.tech@gmail.com>
2024-04-14 20:12:10 +0900

    refactor: 토큰 추출 방식 수정
    - Authorization이 아니라 Cookie로 직접 토큰을 가져오도록 수정

--- projects/paygo-be/src/main/java/mju/paygo/member/ui/auth/support/auth/AuthenticationExtractor.java
+++ projects/paygo-be/src/main/java/mju/paygo/member/ui/auth/support/auth/AuthenticationExtractor.java
@@ -1,35 +1,20 @@
 package mju.paygo.member.ui.auth.support.auth;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.util.StringUtils;
+import mju.paygo.member.exception.exceptions.auth.LoginInvalidException;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class AuthenticationExtractor {
 
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String BEARER = "Bearer";
-    private static final String HEADER_SPLIT_DELIMITER = " ";
-    private static final int TOKEN_TYPE_INDEX = 0;
-    private static final int TOKEN_VALUE_INDEX = 1;
-    private static final int VALID_HEADER_SPLIT_LENGTH = 2;
-
     public static Optional<String> extract(final HttpServletRequest request) {
-        String header = request.getHeader(AUTHORIZATION_HEADER);
+        Cookie[] cookies = request.getCookies();
+        Cookie tokenCookie = Arrays.stream(cookies).filter(cookie -> cookie.getName().equals("token"))
+                .findAny()
+                .orElseThrow(LoginInvalidException::new);
 
-        if (!StringUtils.hasText(header)) {
-            return Optional.empty();
-        }
-
-        return extractFromHeader(header.split(HEADER_SPLIT_DELIMITER));
-    }
-
-    public static Optional<String> extractFromHeader(final String[] headerParts) {
-        if (headerParts.length == VALID_HEADER_SPLIT_LENGTH &&
-                headerParts[TOKEN_TYPE_INDEX].equals(BEARER)) {
-            return Optional.ofNullable(headerParts[TOKEN_VALUE_INDEX]);
-        }
-
-        return Optional.empty();
+        return Optional.of(tokenCookie.getValue());
     }
 }
