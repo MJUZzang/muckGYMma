cd11c50cb89a8cf61bd2bbafdf0d9df30d9d8393
Jehee Cheon <jeheecheon@gmail.com>
2024-04-14 19:41:59 +0900

    refactor: 이미지 분석 요청 시에 credential 포함하도록 수정

--- projects/fe/app/_components/FoodPicture.tsx
+++ projects/fe/app/_components/FoodPicture.tsx
@@ -24,6 +24,7 @@ function FoodPicture(props: Props) {
 
             fetch(`${backendUrl}/api/food/predict`, {
                 method: "POST",
+                credentials: "include",
                 body: formData,
             })
                 .then((res) => {
