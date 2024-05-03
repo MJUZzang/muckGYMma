f6eeae3241a18ddabd54bbd324db9ff6f93f1db5
Jehee Cheon <jeheecheon@gmail.com>
2024-05-04 00:12:00 +0900

    feat: 이얍

--- projects/fe/app/profile/completed/page.tsx
+++ projects/fe/app/profile/completed/page.tsx
@@ -8,14 +8,14 @@ function Page() {
                 {Array.from({ length: 5 }).map((_, i) => (
                     <div
                         key={i}
-                        className="pl-2 pr-4 py-2 flex items-center gap-2 border-b-white/30 border-b-[1px] hover:bg-white/10 transition duration-200"
+                        className="pl-2 pr-4 py-2 flex items-center gap-2 border-b-white/10 border-b-[1px] hover:bg-white/10 transition duration-200"
                     >
                         <Muscle className="" />
                         <div>
                             <p className="text-white/90">Gym workout</p>
                             <p className="text-white/90">320kcal</p>
                             <p className="text-white/90">
-                                Completed: 2022-01-01
+                                완료일: 2022-01-01
                             </p>
                         </div>
                     </div>
