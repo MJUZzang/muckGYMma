a2d42cb0d5de4f8ed8f3cd589935804637a3917d
Jehee Cheon <jeheecheon@gmail.com>
2024-05-23 10:14:36 +0900

    옮겨옮겨

--- projects/fe/app/prediction/plan-selection/page.tsx
+++ projects/fe/app/prediction/plan-selection/page.tsx
@@ -1,41 +1,38 @@
 "use client";
 
-import React, { use, useEffect, useRef, useState } from "react";
+import React, { use, useEffect, useMemo, useRef, useState } from "react";
 import { useAppSelector } from "@/../lib/hooks";
 import { selectPredictResult } from "@/../lib/slices/predictSlice";
 import { backendUrl } from "@/_utils/urls";
 
 function PlanSelectionPage() {
     const predictResult = useAppSelector(selectPredictResult);
-    const [promiseRef, setPromiseRef] = useState<Promise<void> | null>(null);
-
-    if (promiseRef) use(promiseRef!);
-
-    useEffect(() => {
-        if (!promiseRef) {
-            setPromiseRef(
-                fetch(`${backendUrl}/api/task/ask/${predictResult?.id}`, {
-                    method: "POST",
-                    credentials: "include",
-                    cache: "force-cache",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
+    const selectionPromise = useMemo(
+        () =>
+            fetch(`${backendUrl}/api/task/ask/${predictResult?.id}`, {
+                method: "POST",
+                credentials: "include",
+                cache: "force-cache",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((res) => {
+                    if (res.ok) {
+                        return res.json();
+                    } else {
+                        throw new Error("");
+                    }
                 })
-                    .then((res) => {
-                        if (res.ok) {
-                            return res.json();
-                        } else {
-                            throw new Error("");
-                        }
-                    })
-                    .then((data) => {
-                        console.log(data);
-                    })
-                    .catch((err) => console.error(err))
-            );
-        }
-    }, []);
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((err) => console.error(err)),
+        [predictResult?.id]
+    );
+
+    use(selectionPromise);
+
     return <div>PlanSelectionPage</div>;
 }
 
