8f4574f9f44995a33c3f1fd7f817cb555c9fe07b
Jehee Cheon <jeheecheon@gmail.com>
2024-05-25 02:34:21 +0900

    qweasd

--- projects/fe/app/dev/initialize-userinfo/route.ts
+++ projects/fe/app/dev/initialize-userinfo/route.ts
@@ -1,29 +1,58 @@
 import { backendUrl } from "@/_utils/urls";
 import { NextResponse, type NextRequest } from "next/server";
+import { userInfoState } from "../../../lib/slices/userInfoSlice";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
+const dummyUserInfo: userInfoState = {
+    nickname: "JoneDoe",
+    exercises: ["스쿼트", "데드리프트", "벤치프레스"],
+    exerciseSetting: {
+        experience: "3개월 미만",
+        frequency: "주 3회",
+        goal: "근비대",
+        level: "입문자",
+    },
+    physicalSetting: {
+        birth: "1996-01-01",
+        gender: "남성",
+        height: 180,
+        weight: 70,
+    },
+    sports: ["축구"],
+};
+
 export async function GET(request: NextRequest) {
-    return NextResponse.next();
-    // return await fetch(`${backendUrl}/api/login`, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ code, provider }),
-    // })
-    //     .then((res) => {
-    //         if (res.status === 200) {
-    //             const cookies = res.headers.get("Set-Cookie");
-    //             return NextResponse.redirect(new URL("/", request.url), {
-    //                 headers: {
-    //                     "Set-Cookie": cookies || "" // Ensure that the value is not null
-    //                 },
-    //             });
-    //         }
-    //         else {
-    //             return NextResponse.redirect(new URL("/sign-in", request.url));
-    //         }
-    //     })
-    //     .catch(err => console.error(err));
+    if (process.env.NODE_ENV === "development") {
+        return await fetch(`${backendUrl}/api/member/setup`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: request.cookies
+                    .getAll()
+                    .map((cookie) => {
+                        return `${cookie.name}=${cookie.value}`;
+                    })
+                    .join("; "),
+            },
+            body: JSON.stringify(dummyUserInfo),
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("Failed to set up");
+                } else {
+                    console.log("Successfully set up");
+                    return NextResponse.redirect(
+                        new URL("/main/workout", request.url)
+                    );
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                return NextResponse.error();
+            });
+    } else {
+        return NextResponse.redirect(new URL("/main/workout", request.url));
+    }
 }
