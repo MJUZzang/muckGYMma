b0220790ffb6811af8c042b0a62dd274a7dc8a3d
Jehee Cheon <jeheecheon@gmail.com>
2024-05-25 12:36:59 +0900

    asd

--- projects/fe/app/plan/pick/[mealId]/page.tsx
+++ projects/fe/app/plan/pick/[mealId]/page.tsx
@@ -5,51 +5,53 @@ import { useAppSelector } from "@/../lib/hooks";
 import { selectPredictResult } from "@/../lib/slices/predictSlice";
 import { backendUrl } from "@/_utils/urls";
 
-function PlanSelectionPage() {
+function PickPage() {
     const predictResult = useAppSelector(selectPredictResult);
-    const [plansPromise, setPlansPromise] = useState<Promise<void> | null>(null);
+    const [plansPromise, setPlansPromise] = useState<Promise<void> | null>(
+        null
+    );
 
     if (plansPromise) use(plansPromise!);
 
     useEffect(() => {
         if (!plansPromise) {
-            // setPromiseRef(
-            //     fetch(`${backendUrl}/api/task/ask/${predictResult?.id}`, {
-            //         method: "POST",
-            //         credentials: "include",
-            //         cache: "force-cache",
-            //         headers: {
-            //             "Content-Type": "application/json",
-            //         },
-            //     })
-            //         .then((res) => {
-            //             if (res.ok) {
-            //                 return res.json();
-            //             } else {
-            //                 throw new Error("");
-            //             }
-            //         })
-            //         .then((data) => {
-            //             console.log(data);
-            //         })
-            //         .catch((err) => console.error(err))
-            // );
             setPlansPromise(
-                new Promise((res, rej) => {
-                    setTimeout(() => {}, 60000);
+                fetch(`${backendUrl}/api/task/ask/${predictResult?.id}`, {
+                    method: "POST",
+                    credentials: "include",
+                    cache: "force-cache",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
                 })
+                    .then((res) => {
+                        if (res.ok) {
+                            return res.json();
+                        } else {
+                            throw new Error("");
+                        }
+                    })
+                    .then((data) => {
+                        console.log(data);
+                    })
+                    .catch((err) => console.error(err))
             );
+            // setPlansPromise(
+            //     new Promise((res, rej) => {
+            //         setTimeout(() => {}, 60000);
+            //     })
+            // );
         }
     }, []);
 
     // if (!predictResult?.id) {
     //     throw new Error("Predict result is not found");
     // } else
-     if (!plansPromise) {
+    if (!plansPromise) {
         return null;
     } else {
-        return <div className="text-red-500">PlanSelectionPage</div>;
+        return <div className="text-red-500">Fetched successfully</div>;
     }
 }
 
-export default PlanSelectionPage;
+export default PickPage;
