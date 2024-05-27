1c4f05364b544ef9bb4663bff82fe6db31d146f8
Jehee Cheon <jeheecheon@gmail.com>
2024-05-27 12:42:44 +0900

    asd

--- projects/fe/app/_components/UploadMeal.tsx
+++ projects/fe/app/_components/UploadMeal.tsx
@@ -34,6 +34,9 @@ function UploadMeal({ className, buttonContent: buttonName }: UploadMealProps) {
             console.error("No image data found.");
             return;
         }
+        
+        console.log(src);
+
         const file = src!;
         const formData = new FormData();
         formData.append("file", file);
