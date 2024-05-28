ba6943b2e90c8cd05bdaffb8999d8b190fd04d80
Jehee Cheon <jeheecheon@gmail.com>
2024-05-28 13:11:16 +0900

    q

--- projects/fe/app/plan/info/[planId]/page.tsx
+++ projects/fe/app/plan/info/[planId]/page.tsx
@@ -153,7 +153,7 @@ function InfoPage() {
                                 </p>
                                 &nbsp;kcal
                             </div>
-                            {planInfo.cleared && (
+                            {planInfo.clearedAt && (
                                 <p>{`완료일: ${planInfo.clearedAt}`}</p>
                             )}
                         </div>
