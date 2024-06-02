622c65e6955854d6854c5b44fc55e7a9439458f3
Jehee Cheon <jeheecheon@gmail.com>
2024-06-03 05:52:50 +0900

    asd

--- projects/fe/app/post/[postId]/page.tsx
+++ projects/fe/app/post/[postId]/page.tsx
@@ -2,6 +2,7 @@ import { PostInfo } from "@/_types/PostInfo";
 import { backendUrl } from "@/_utils/urls";
 import { FetchNickname } from "@/_utils/user";
 import Post from "@/main/_components/Post";
+import NavigateBack from "@/main/profile/[username]/(follow)/_components/NavigateBack";
 import { cookies } from "next/headers";
 import React, { useEffect } from "react";
 
@@ -55,7 +56,13 @@ async function PostPage({ params }: PostPageProps) {
         return "Failed to fetch post";
     }
     return (
-        <div>
+        <div className="fixed top-0 h-[100dvh]">
+            <div className="grid grid-cols-3 px-2 py-2">
+                <NavigateBack />
+                <p className="w-full text-center text-lg">좋아요</p>
+                <div />
+            </div>
+
             <Post postInfo={post} />
         </div>
     );
