ad920187db33d29aefd9a4081980a3b19efee540
Jehee Cheon <jeheecheon@gmail.com>
2024-06-02 16:16:28 +0900

    a

--- projects/fe/app/main/community/following/page.tsx
+++ projects/fe/app/main/community/following/page.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Post from "@/main/_components/Post";
 
-import { fetchFollowingPosts } from "@/_utils/post";
+import { fetchFollowingPosts, sortPostsByDate } from "@/_utils/post";
 
 async function FollowingPage() {
     const posts = await fetchFollowingPosts();
-
+    const sortedPosts = sortPostsByDate(posts)
+    
     return (
         <>
             <div className="flex flex-col items-center pt-3 pb-6 h-full gap-3 bg-app-bg-1">
-                {posts.map((post, i) => (
+                {sortedPosts.map((post, i) => (
                     <Post postInfo={post} key={i} />
                 ))}
             </div>
--- projects/fe/app/main/community/recent/page.tsx
+++ projects/fe/app/main/community/recent/page.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Post from "@/main/_components/Post";
 
-import { fetchRecentPosts } from "@/_utils/post";
+import { fetchRecentPosts, sortPostsByDate } from "@/_utils/post";
 
 async function RecentPage() {
     const posts = await fetchRecentPosts();
+    const sortedPosts = sortPostsByDate(posts)
 
     return (
         <>
             <div className="flex flex-col items-center pt-3 pb-6 h-full gap-3 bg-app-bg-1">
-                {posts.map((post, i) => (
+                {sortedPosts.map((post, i) => (
                     <Post postInfo={post} key={post.id} />
                 ))}
             </div>
